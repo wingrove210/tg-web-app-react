8c487b044f8b469f713ef724829c2d8c82fd3ea8
Vlad Dremkov <vladidermkov330@gmail.com>
2025-01-26 09:52:04 +0300

    modified:   src/App.js 	modified:   src/components/Header/Header.jsx 	new file:   src/hooks/useTelegram.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import { useTelegram } from './hooks/useTelegram';
 function App() {
-
+  const {tg, onToggleButton} = useTelegram();
+  useEffect(()=>{
+    tg.ready();
+  }, [])
   return (
     <div className="App">
         <Header />
+        <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import Button from "../Button/Button";
-// import {useTelegram} from "../../hooks/useTelegram";
+import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
-
 const Header = () => {
-    const tg = window.Telegram.WebApp;
-    // const {user, onClose} = useTelegram();
-    const onClose = () => {
-        tg.close();
-    }
+    const {user, onClose} = useTelegram();
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {/* {user?.username} */}
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     );

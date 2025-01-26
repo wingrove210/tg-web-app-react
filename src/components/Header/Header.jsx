a2517bd9c3dce8400f9818829c68135710069589
Vlad Dremkov <vladidermkov330@gmail.com>
2025-01-26 09:21:58 +0300

    new file:   src/components/Button/Button.css 	new file:   src/components/Button/Button.jsx 	new file:   src/components/Form/Form.css 	new file:   src/components/Form/Form.jsx 	new file:   src/components/Header/Header.css 	new file:   src/components/Header/Header.jsx 	new file:   src/components/ProductItem/ProductItem.css 	new file:   src/components/ProductItem/ProductItem.jsx 	new file:   src/components/ProductList/ProductList.css 	new file:   src/components/ProductList/ProductList.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from "../Button/Button";
+// import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+    // const {user, onClose} = useTelegram();
+    const onClose = () => {
+        tg.close();
+    }
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {/* {user?.username} */}
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;

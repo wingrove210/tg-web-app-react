48f29b78f4a36ab53195446826d31239c37494f9
Vlad Dremkov <vladidermkov330@gmail.com>
2025-01-26 14:55:15 +0300

    modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -38,7 +38,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [addedItems]);
+    }, [addedItems, queryId]);
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)

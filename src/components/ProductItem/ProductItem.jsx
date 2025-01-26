8ed212e55f14b612826b67db56186f3eee8b8a52
Vlad Dremkov <vladidermkov330@gmail.com>
2025-01-26 11:28:21 +0300

    modified:   src/components/ProductItem/ProductItem.jsx 	modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Button from "../Button/Button";
+import './ProductItem.css';
 
-const ProductItem = () => {
+const ProductItem = ({product, className, onAdd}) => {
+
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+
     return (
-        <div>
-            
+        <div className={'product ' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
         </div>
     );
 };
